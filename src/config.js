c1649a766215062bd47b2030cac0c0253238be1b
stevenwthornton0 <stevenwthornton0@gmail.com>
2020-02-12 17:29:46 -0600

    updated database

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 
 export default {
-    API_ENDPOINT: 'postgres://lttgfwcqezjxpz:ba9246a002eedaa1630d4ac701482fa0bac0f5ac6de668da00f8632fa3aa2071@ec2-18-213-176-229.compute-1.amazonaws.com:5432/dcbtpsk5ms0126',
+    API_ENDPOINT: 'https://shielded-island-54017.herokuapp.com',
     TOKEN_KEY: 'servicepro-client-auth-token',
     USER_KEY: 'servicepro-user-auth-token'
 }
